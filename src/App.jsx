6a0a4406966940b1f926dedefbc9d3c3d69f3796
Khalid-Parvaiz <khalidparvaiz917@gmail.com>
2026-04-13 02:36:03 +0500

    create and deploy sign up and login page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Login from './view/login'
+import SignUp from './view/signup'
+
+function App() {
+
+  return (
+    <Login />
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import SignUp from './view/signup/index.jsx'
+import { ToastContainer } from 'react-toastify'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/ecommerce-store" element={<App />} />
+      <Route path="/ecommerce-store/signUp" element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
+  <ToastContainer />
+  </StrictMode>,
+)
